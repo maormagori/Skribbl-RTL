961f6990b348822cc1f4f0c07f7918f64edcabfc
maormagori <maormagori@gmail.com>
2020-09-24 19:06:32 +0300

    Added comments.

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,34 +2,40 @@
 
 console.log("skribbl RTL has started.")
 
+//Fetching the current word node
 const currentWordNode = document.getElementById("currentWord");
 
+//The observer config
 const config = { characterData: true, childList: true};
 
-
+//The observer's callback. called each time the inner html of current word changes.
 const whenWordChanges = () =>{
     
-    console.log("A mutatiםn has occured.")
+    //The chagnged word.
     let word = currentWordNode.innerHTML;
-    console.log(`the word has been changed: ${word}`)
+
+    //Checking if it contains rtl characters. Atm skribbl only has one rtl language which is hebrew.
     if (containsHebrew(word)){
-        console.log(`the word contains hebrew.`)
+        // If the word contains hebrew characters we change it's direction to rtl. 
         currentWordNode.style.direction = "rtl";
     }
+    //If the word does not contain rtl characters and the current direction is rtl we change it back to ltr.
     else if(currentWordNode.style.direction === "rtl"){
-        console.log("the word does not contain hebrew.")
         currentWordNode.style.direction = "ltr";
     }
     
     
 }
 
+//Runs a regex test on word and checks if it contains any unicode symbols from hebrew.
 const containsHebrew = (word) => {
     return ((/[\u0590-\u05FF]/).test(word));
 }
 
+//Initializing the observer.
 const observer = new MutationObserver(whenWordChanges);
 
+//Running the observer.
 observer.observe(currentWordNode, config);
 
 
