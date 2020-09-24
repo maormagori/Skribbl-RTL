22de7c2c6a2317b3d42e4c992d6361d9d152812c
maormagori <maormagori@gmail.com>
2020-09-24 18:55:32 +0300

    finished

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,36 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+console.log("skribbl RTL has started.")
+
+const currentWordNode = document.getElementById("currentWord");
+
+const config = { characterData: true, childList: true};
+
+
+const whenWordChanges = () =>{
+    
+    console.log("A mutatiםn has occured.")
+    let word = currentWordNode.innerHTML;
+    console.log(`the word has been changed: ${word}`)
+    if (containsHebrew(word)){
+        console.log(`the word contains hebrew.`)
+        currentWordNode.style.direction = "rtl";
+    }
+    else if(currentWordNode.style.direction === "rtl"){
+        console.log("the word does not contain hebrew.")
+        currentWordNode.style.direction = "ltr";
+    }
+    
+    
+}
+
+const containsHebrew = (word) => {
+    return ((/[\u0590-\u05FF]/).test(word));
+}
+
+const observer = new MutationObserver(whenWordChanges);
+
+observer.observe(currentWordNode, config);
+
+
+
